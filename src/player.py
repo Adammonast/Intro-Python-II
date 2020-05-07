3dcac8a3a7feec0d5342ad85aa0f10acff651878
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-07 13:36:12 -0400

    added Room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Room:
+    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+
+    def __str__(self):
+        return f"You are in the {self.name} room. {self.description}"
